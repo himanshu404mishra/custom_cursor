f92d41b088b7760d019ca7a3041616f2893d9a9e
Himanshu Mishra <himanshu404mishra@gmail.com>
2024-08-13 20:37:49 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let main = document.querySelector("#main")
+let cur = document.querySelector("#cursor")
+
+main.addEventListener("mousemove", function(dets){
+    cur.style.left = dets.x+"px"
+    cur.style.top = dets.y+"px"
+})  
